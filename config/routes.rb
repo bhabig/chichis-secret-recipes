2f80131891e042cfac280e5b80aa73065c3aa371
Bob Habig <rfhab2@gmail.com>
2017-02-20 21:35:44 -0500

    added nested route options plus unnested options

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#welcome'
-  
+
   resources :users
+
   resources :users do
     resources :recipes
   end
 
+  resources :recipes
+
+  resources :recipes do
+    resources :blends
+    resources :ingredients
+  end
+
+  resources :blends do
+    resources :ingredients
+  end
+
+  resources :ingredients
+
+  resources :blends
+
   resources :sessions, only: [:new, :create, :destroy]
+  
 end
