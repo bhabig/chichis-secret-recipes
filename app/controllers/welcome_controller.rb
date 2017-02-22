ad00e2d2ccda772150cedb313f005c6a241470d2
Bob Habig <rfhab2@gmail.com>
2017-02-21 23:19:03 -0500

    css done for signed in. just add links to _header-links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :current_user
+
   def welcome
+    if current_user
+      redirect_to user_path(current_user)
+    else
+      render :welcome
+    end
   end
 end
