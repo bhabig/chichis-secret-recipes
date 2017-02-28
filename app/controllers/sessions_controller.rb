7db8b65a6afe3cea7f22329fb6846ab6ca2bcb80
Bob Habig <rfhab2@gmail.com>
2017-02-28 15:47:23 -0500

    omniauth login groundwork

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,20 +10,40 @@ class SessionsController < ApplicationController
     end
   end
 
+=begin
   def facebook
     omni_hash = request.env["omniauth.auth"]
     user = User.find_or_create_by_omniauth(omni_hash)
     session[:user_id] = user.try(:id)
     redirect_to root_path
   end
+=end
 
   def create
-    @user = User.where("lower(name) = ?", params[:user][:name].downcase).first
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if auth
+      omni_email = auth["info"]["email"]
+      @user = User.find_by(email: omni_email)
+      if @user
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        omni_name = auth["info"]["name"]
+        @user = User.new(email: omni_email, password: SecureRandom.hex, name: omni_name)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          redirect_to root_path, alert: "#{@user.errors.full_messages.first}"
+        end
+      end
     else
-      redirect_to new_session_path, alert: "information provided invalid"
+      @user ||= User.where("lower(name) = ?", params[:user][:name].downcase).first
+      if @user && user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to new_session_path, alert: "information provided invalid"
+      end
     end
   end
 
