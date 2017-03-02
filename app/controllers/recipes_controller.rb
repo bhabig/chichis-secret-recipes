1a19f70f90195cb7678c5bd9d24b4f769a438ee0
Bob Habig <rfhab2@gmail.com>
2017-03-01 23:09:38 -0500

    retooled index view for recipes, added rest of actions to controller (need content)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,6 +27,22 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
   private
 
   def recipe_params
