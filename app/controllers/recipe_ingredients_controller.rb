e0340ff9ee8acf21acaaf929097ffa1968c9afac
Bob Habig <rfhab2@gmail.com>
2017-03-08 15:46:13 -0500

    changed recipes_for_ingredients to recipe_for_ingredient and added recipe delete button on show page

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -2,7 +2,7 @@ class RecipeIngredientsController < ApplicationController
   def index
     if logged_in? && current_user
       @ingredient = Ingredient.find_by(id: params['ingredient_id'].to_i)
-      @recipes = RecipeIngredient.recipes_for_ingredients(@ingredient.id)
+      @recipes = RecipeIngredient.recipes_for_ingredient(@ingredient.id)
     else
       redirect_to root_path, alert: "login first"
     end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -4,7 +4,7 @@ class RecipeIngredient < ApplicationRecord
 
   #method to convert category integer into category type to display
 
-  def self.recipes_for_ingredients(ing_id)
+  def self.recipes_for_ingredient(ing_id)
     recipe_ids = self.all.map{|ri| ri.recipe_id if ri.ingredient_id == ing_id}
     @recipes = recipe_ids.compact.map{|id| Recipe.find_by(id: id)}
   end
