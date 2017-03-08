b61e1fc9b6db38b13fe976fbbbc2340f7dc4c922
Bob Habig <rfhab2@gmail.com>
2017-03-08 15:19:32 -0500

    refactored recipes display for given ing into RecipeIngredient class method .recipes_for_ingredients

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -2,8 +2,7 @@ class RecipeIngredientsController < ApplicationController
   def index
     if logged_in? && current_user
       @ingredient = Ingredient.find_by(id: params['ingredient_id'].to_i)
-      recipe_ids = RecipeIngredient.all.map{|ri| ri.recipe_id if ri.ingredient_id == params['ingredient_id'].to_i}
-      @recipes = recipe_ids.compact.map{|id| Recipe.find_by(id: id)}
+      @recipes = RecipeIngredient.recipes_for_ingredients(@ingredient.id)
     else
       redirect_to root_path, alert: "login first"
     end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,10 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
 
   #method to convert category integer into category type to display
+
+  def self.recipes_for_ingredients(ing_id)
+    recipe_ids = self.all.map{|ri| ri.recipe_id if ri.ingredient_id == ing_id}
+    @recipes = recipe_ids.compact.map{|id| Recipe.find_by(id: id)}
+  end
+
 end
