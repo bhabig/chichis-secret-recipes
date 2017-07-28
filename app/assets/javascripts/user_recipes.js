3f9b8a8193a5184802db1daf28506e1abb0868bd
Bob Habig <rfhab2@gmail.com>
2017-07-28 08:46:01 -0400

    built es6 class for Recipe and made functions on prototype inside class but outside constructor

--- app/assets/javascripts/user_recipes.js
+++ app/assets/javascripts/user_recipes.js
@@ -1,40 +1,30 @@
-$(function() {
-  $('#user-recipes').click(function(e) {
-    e.preventDefault();
-    getUsersRecipes(this)
-  });
-  //COMMENTS ARE FOR CERNAN SPECIFICALLY TO ILLUSTRATE THE CHANGES MADE
-
-  //new constructor function for recipe
-  function Recipe(name, picture, id, user_id) {
+class Recipe {
+  constructor(name, picture, id, user_id) {
     this.name = name;
     this.picture = picture;
     this.id = id;
     this.user_id = user_id;
-    // this.formatName = formatName; //formatter?
-    // this.formatPic = formatPic; //formatter?
-    // this.formatLink = formatLink; //formatter?
   }
-
-  //NEW...formatter?
-  Recipe.prototype.formatName = function() {
+  formatName() {
     return '<h4>' + this.name + '</h4>';
   }
-
-  //used to be called imageFilter(image)...formatter?
-  Recipe.prototype.formatPic = function() {
+  formatPic() {
     if (this.picture.includes("/system/recipes/")) {
       return "<img class='thumb' src='" + this.picture + "'></br>"
     } else {
       return "<img class='thumb' src='/assets/" + this.picture + "'></br>";
     }
   }
-
-  //NEW...formatter?
-  Recipe.prototype.formatLink = function() {
+  formatLink() {
     return "<a class='nav-link' href='/users/" + this.user_id + "/recipes/" + this.id + "'>VIEW RECIPE</a></br>"
   }
+}
 
+$(function() {
+  $('#user-recipes').click(function(e) {
+    e.preventDefault();
+    getUsersRecipes(this)
+  });
 
   function getUsersRecipes(obj) {
     var x = obj.href.split('/');
@@ -44,18 +34,14 @@ $(function() {
         $('h3').removeClass('i-hide');
         response.recipes.forEach(function(recipe) {
 
-          //this li variable didn't exist before and was appended directly in the .append() function at the bottom of this // //response function
           var $li = $(document.createElement('li'));
 
-          //this object model prototype is new and is replacing the more 'manual' way of doing it. there is a key for each //trait value and a function for each html element that needed to be created and appended to the recipe <li>
           var buildRecipe = new Recipe(recipe.name.toUpperCase(), recipe.recipe_avatar, recipe.id, recipe.user_id);
-
-          //the lines below used to be the lines commented at the bottom of the file
+          debugger;
           $li.append(buildRecipe.formatName());
           $li.append(buildRecipe.formatPic());
           $li.append(buildRecipe.formatLink());
 
-          //this used to be: $('#user-recipe-drop').append('<li>' + nameTag + picTag + showLinkTag + '</li>')
           $('#user-recipe-drop').append($li);
         });
       }
