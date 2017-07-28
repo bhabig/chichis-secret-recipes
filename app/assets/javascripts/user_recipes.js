69e87db14c9ecc12b9ca24c0014603f0127d0f0c
Bob Habig <rfhab2@gmail.com>
2017-07-27 20:15:26 -0400

    commented out my user-recipes js file for Cernan

--- app/assets/javascripts/user_recipes.js
+++ app/assets/javascripts/user_recipes.js
@@ -3,21 +3,25 @@ $(function() {
     e.preventDefault();
     getUsersRecipes(this)
   });
+  //COMMENTS ARE FOR CERNAN SPECIFICALLY TO ILLUSTRATE THE CHANGES MADE
 
+  //new constructor function for recipe
   function Recipe(name, picture, id, user_id, formatName, formatPic, formatLink) {
     this.name = name;
     this.picture = picture;
     this.id = id;
     this.user_id = user_id;
-    this.formatName = formatName;
-    this.formatPic = formatPic;
-    this.formatLink = formatLink;
+    this.formatName = formatName; //formatter?
+    this.formatPic = formatPic; //formatter?
+    this.formatLink = formatLink; //formatter?
   }
 
+  //NEW...formatter?
   function formatName() {
     return '<h4>' + this.name + '</h4>';
   }
 
+  //used to be called imageFilter(image)...formatter?
   function formatPic() {
     if (this.picture.includes("/system/recipes/")) {
       return "<img class='thumb' src='" + this.picture + "'></br>"
@@ -26,6 +30,7 @@ $(function() {
     }
   }
 
+  //NEW...formatter?
   function formatLink() {
     return "<a class='nav-link' href='/users/" + this.user_id + "/recipes/" + this.id + "'>VIEW RECIPE</a></br>"
   }
@@ -39,14 +44,18 @@ $(function() {
         $('h3').removeClass('i-hide');
         response.recipes.forEach(function(recipe) {
 
+          //this li variable didn't exist before and was appended directly in the .append() function at the bottom of this // //response function
           var $li = $(document.createElement('li'));
-          
+
+          //this object model prototype is new and is replacing the more 'manual' way of doing it. there is a key for each //trait value and a function for each html element that needed to be created and appended to the recipe <li>
           var buildRecipe = new Recipe(recipe.name.toUpperCase(), recipe.recipe_avatar, recipe.id, recipe.user_id, formatName, formatPic, formatLink);
 
+          //the lines below used to be the lines commented at the bottom of the file
           $li.append(buildRecipe.formatName());
           $li.append(buildRecipe.formatPic());
           $li.append(buildRecipe.formatLink());
 
+          //this used to be: $('#user-recipe-drop').append('<li>' + nameTag + picTag + showLinkTag + '</li>')
           $('#user-recipe-drop').append($li);
         });
       }
@@ -54,7 +63,7 @@ $(function() {
   };
 });
 
-
+// NOT USED ANYMORE, USED TO BE IN RESPONSE FUNCTION
 // var nameTag = '<h4>' + recipe.name.toUpperCase() + '</h4>';
 // var picTag =  imageFilter(recipe);
 // var showLinkTag = "<a class='nav-link' href='/users/" + recipe.user_id + "/recipes/" + recipe.id + "'>VIEW RECIPE</a></br>"
